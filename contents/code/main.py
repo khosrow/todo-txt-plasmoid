1bba55283fb41e0c19965812b74e35525d8d7543
Khosrow E <khosrow.ebrahimpour@gmail.com>
2011-02-22 21:00:18 -0500

    config dialog now displays properly

--- contents/code/main.py
+++ contents/code/main.py
@@ -1,74 +1,87 @@
 # -*- coding: utf-8 -*-
 # <Copyright and license information goes here.>
-from PyQt4.QtCore import Qt, QPoint
-from PyQt4.QtGui import QPen
+#from PyQt4.QtCore import Qt, QPoint
+from PyQt4.QtCore import *
+#from PyQt4.QtGui import QPen
+from PyQt4.QtGui import *
 from PyKDE4.plasma import Plasma
-from PyKDE4 import plasmascript
-import commands
+from PyKDE4 import plasmascript, kdeui, kdecore
+from PyKDE4.kdeui import *
 import string
-import todo
- 
-class HelloPython(plasmascript.Applet):
-    _todolist = ""
-    def __init__(self,parent,args=None):
-        plasmascript.Applet.__init__(self,parent)
-        self._todolist = todo.TodoList("/home/kandn/Documents/todo/todo.txt")
- 
-    def init(self):
-        self.setHasConfigurationInterface(False)
-        self.resize(350, 350)
-        #self.setAspectRatioMode(Plasma.Square)
-        self.setAspectRatioMode(Plasma.IgnoreAspectRatio)
- 
-    def paintInterface(self, painter, option, rect):
-	painter.save()
-        #pen = QPen()                       
-	painter.setPen(Qt.gray)
-	#painter.setPen(Qt.black)
-	painter.drawText(rect, Qt.AlignTop | Qt.AlignHCenter, "Todo List")
-	# get the todo list
-	#(status, output) = commands.getstatusoutput("~/bin/todo.sh -p list")
-	#lines = output.split("\n")
-	
-        todos = self._todolist.getList()
-        
-        ##painter.drawText(rect, Qt.AlignVCenter | Qt.AlignHCenter, "Hello Python!")
-        #painter.drawText(rect, Qt.AlignTop | Qt.AlignJustify, output)
-	
-	point = QPoint(15,50)
-	y = 50
-	
-	for item in todos:
-	    if item.getPriority() == "A":
-		painter.setPen(Qt.yellow)
-	    elif item.getPriority() == "B":
-		painter.setPen(Qt.darkGreen)
-	    elif item.getPriority() == "C":
-		painter.setPen(Qt.darkCyan)
-	    else:
-		painter.setPen(Qt.white)
-	    painter.drawText(point,item.getText())
-	    y = y +15
-	    point.setY(y)
-	    
-	#for line in lines:
-		#words = line.split(" ")
-		#if (len(words) > 1) and (words[1] == "(A)"):
-		  #painter.setPen(Qt.yellow)
-		#elif (len(words) > 1) and (words[1] == "(B)"):
-		  #painter.setPen(Qt.darkGreen)
-		#elif (len(words) > 1) and (words[1] == "(C)"):
-		  #painter.setPen(Qt.darkCyan)
-		#else:
-		  #painter.setPen(Qt.white)
-		#painter.drawText(point, line)
-		#y = y + 15
-		#point.setY(y)
-        painter.restore()
 
-    def createConfigurationInterface(self,parent):
-	print "Hello world"
-	
+from todo import TodoList
+#from configDialog import ConfigDialog
+from configform import ConfigForm
  
+class TodoApplet(plasmascript.Applet):
+	_todolist = ""
+	_settings = {}
+	#_configDialog = ""
+	_configForm = ""
+	def __init__(self,parent,args=None):
+		plasmascript.Applet.__init__(self,parent)
+	
+	def init(self):
+		self.setHasConfigurationInterface(True)
+		#self.resize(350, 350)
+		#self.setAspectRatioMode(Plasma.Square)
+		self.setAspectRatioMode(Plasma.IgnoreAspectRatio)
+		self._settings["todoURL"] = "/home/kandn/Documents/todo/todo.txt"
+		self._settings["priorityAColour"] = ""
+		self._settings["priorityBColour"] = ""
+		self._settings["priorityCColour"] = ""
+		self._settings["priorityDColour"] = ""        
+		self._todolist = TodoList(self._settings["todoURL"])
+	
+	def createConfigurationInterface(self, parent):
+		#self._configDialog = ConfigDialog(parent, self._settings)
+		self._configForm = ConfigForm(parent, self._settings)
+		##parent.setMainWidget(self._configDialog)
+		#page = parent.addPage( self._configDialog, "My Config Page")
+		page = parent.addPage(self._configForm, "todo.txt Config Page")
+		page.setIcon(KIcon("preferences-desktop-color"))
+		self.connect(parent, SIGNAL("okClicked()"), self.configAccepted)
+	
+	def showConfigurationInterface(self):
+		dialog = KPageDialog()
+		dialog.setFaceType(KPageDialog.List)
+		dialog.setButtons(KDialog.ButtonCode(KDialog.Ok | KDialog.Cancel))
+		self.createConfigurationInterface(dialog)
+		dialog.exec_()
+	
+	def configAccepted(self):
+		#self._settings = self._configDialog.getSettings()
+		self._settings = self._configForm.getSettings()
+		self._todolist.refreshList()
+	
+	def paintInterface(self, painter, option, rect):
+		painter.save()
+		#pen = QPen()                       
+		painter.setPen(Qt.gray)
+		#painter.setPen(Qt.black)
+		painter.drawText(rect, Qt.AlignTop | Qt.AlignHCenter, "Todo List")
+		#todos = self._todolist.getList()
+		todos = self._todolist.sortList()
+		##painter.drawText(rect, Qt.AlignVCenter | Qt.AlignHCenter, "Hello Python!")
+		#painter.drawText(rect, Qt.AlignTop | Qt.AlignJustify, output)
+		point = QPoint(15,50)
+		y = 50
+		for item in todos:
+			# find the color to use for each priority
+			if item.getPriority() == "A":
+				painter.setPen(Qt.yellow)
+			elif item.getPriority() == "B":
+				painter.setPen(Qt.darkGreen)
+			elif item.getPriority() == "C":
+				painter.setPen(Qt.darkCyan)
+			else:
+				painter.setPen(Qt.white)
+			# now draw the text in the widget
+			painter.drawText(point,item.getText())
+			y = y +15
+			point.setY(y)
+		painter.restore()
+	
+
 def CreateApplet(parent):
-    return HelloPython(parent)
+	return TodoApplet(parent)
